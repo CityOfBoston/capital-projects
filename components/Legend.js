d47f6707c2e500e53cdbb82a27fd0bd178a8b01b
Kayla Patel <paylakatel@gmail.com>
2019-03-31 12:03:26 -0400

    adding new datasources, ui updates

--- components/Legend.js
+++ components/Legend.js
@@ -5,6 +5,12 @@ export default function Legend() {
   return (
     <div>
       <Card className="mt-1 border-0">
+        <Card className="border-0 pt-2 ml-1 mr-2">
+          <h5 className="text-uppercase" style={{ letterSpacing: '1px' }}>
+            Capital Projects
+          </h5>
+        </Card>
+
         <Row>
           <img
             src="/capital-projects/static/blueCircle.svg"
@@ -12,7 +18,7 @@ export default function Legend() {
             height="25px"
             className="ml-3 mr-1"
           />
-          <p className="font-italic">Capital Budget Projects</p>
+          <p className="font-italic">Annual or Ongoing Programs</p>
         </Row>
 
         <Row>
@@ -22,7 +28,7 @@ export default function Legend() {
             height="25px"
             className="ml-3 mr-1"
           />
-          <p className="font-italic">Pedestrian Ramps</p>
+          <p className="font-italic">Projects being planned</p>
         </Row>
 
         <Row>
@@ -32,7 +38,17 @@ export default function Legend() {
             height="25px"
             className="ml-3 mr-1"
           />
-          <p className="font-italic">Street Projects</p>
+          <p className="font-italic">Projects being designed</p>
+        </Row>
+
+        <Row>
+          <img
+            src="/capital-projects/static/line.svg"
+            width="25px"
+            height="25px"
+            className="ml-3 mr-1"
+          />
+          <p className="font-italic">Projects in construction</p>
         </Row>
       </Card>
     </div>
